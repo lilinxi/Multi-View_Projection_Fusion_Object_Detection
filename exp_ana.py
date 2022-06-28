c20caa2de94918594ed9ec86aa2fa07a88374ec2
limengfan.lmf <limengfan.lmf@bytedance.com>
2022-06-28 19:16:50 +0800

    commit at 2022-06-28 19:16:50:

---
+++ exp_ana.py
@@ -0,0 +1,58 @@
+Std = [
+    0.881,
+    0.875,
+    0.710,
+    # 0.599,
+    # 0.595,
+    0.776,
+    # 0.585,
+    # 0.318,
+    0.793,
+    0.648,
+    # 0.474,
+    0.747,
+    0.868,
+]
+
+Def = [
+    0.860,
+    0.870,
+    0.704,
+    # 0.606,
+    # 0.589,
+    0.784,
+    # 0.562,
+    # 0.342,
+    0.812,
+    0.647,
+    # 0.484,
+    0.747,
+    0.890,
+]
+
+Ste = [
+    0.900,
+    0.888,
+    0.720,
+    # 0.608,
+    # 0.575,
+    0.775,
+    # 0.587,
+    # 0.297,
+    0.806,
+    0.654,
+    # 0.468,
+    0.768,
+    0.872,
+]
+
+# for i in range(10):
+#     print('cur', sum(Ste) / len(Ste))
+#     print('rm', min(Ste))
+#     Ste.remove(min(Ste))
+
+print('cur', sum(Std) / len(Std))
+print('cur', sum(Def) / len(Def))
+print('cur', sum(Ste) / len(Ste))
+
+# bed & painting & table & mirror & curtain & sofa & door & bedside & tv &
